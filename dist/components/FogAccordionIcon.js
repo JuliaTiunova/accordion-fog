8566669e1be4e8727520cb6742e7aa8b1f13c3eb
Yuliia1 <yuliia.tiunova@valtech.com>
2023-04-27 21:19:01 +0300

    change arrow icon to font awesome icon

--- dist/components/FogAccordionIcon.js
+++ dist/components/FogAccordionIcon.js
@@ -6,12 +6,15 @@ Object.defineProperty(exports, "__esModule", {
 exports.default = FogAccordionIcon;
 var _react = _interopRequireDefault(require("react"));
 require("./styles/FogAccordionIcon.css");
-var _arrow = require("../arrow.svg");
+var _reactFontawesome = require("@fortawesome/react-fontawesome");
+var _freeSolidSvgIcons = require("@fortawesome/free-solid-svg-icons");
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 function FogAccordionIcon(props) {
   return /*#__PURE__*/_react.default.createElement("button", {
     className: "fog-accordion-button"
   }, /*#__PURE__*/_react.default.createElement("span", {
     className: "fog-accordion-icon "
-  }, props.icon || props.children || /*#__PURE__*/_react.default.createElement(_arrow.ReactComponent, null)));
+  }, props.icon || props.children || /*#__PURE__*/_react.default.createElement(_reactFontawesome.FontAwesomeIcon, {
+    icon: _freeSolidSvgIcons.faArrowDown
+  })));
 }
--- src/lib/components/FogAccordionIcon.js
+++ src/lib/components/FogAccordionIcon.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './styles/FogAccordionIcon.css';
-import { ReactComponent as Arrow } from '../arrow.svg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowDown } from '@fortawesome/free-solid-svg-icons';
 
 export default function FogAccordionIcon(props) {
   return (
     <button className='fog-accordion-button'>
       <span className='fog-accordion-icon '>
-        {props.icon || props.children || <Arrow />}
+        {props.icon || props.children || <FontAwesomeIcon icon={faArrowDown} />}
       </span>
     </button>
   );
